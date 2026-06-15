f42af908bfaa8c3f280ced5d0adba8e897a68580
lisa <liza.serpikova@stud.tsu.ru>
2026-06-15 08:22:09 +0700

    test

--- main.cpp
+++ main.cpp
@@ -14,65 +14,113 @@ QTextStream cout(stdout);
 int main() {
 
     //Генерируем C++ код
-    std::shared_ptr<CppCodeFactory> cppCodeFactory = std::make_shared<CppCodeFactory>();
-    CodeGenerator cppGenerator(cppCodeFactory);
+    cout << "C++ (CppCodeFactory)\n\n" << Qt::endl;
 
-    cppGenerator.addClass("MyClass");
+    std::shared_ptr<CppCodeFactory> cppFactory = std::make_shared<CppCodeFactory>();
 
-    cppGenerator.addMethod("myMethod", "int", CppMethodUnit::STATIC, CppClassUnit::PUBLIC);
-    cppGenerator.addPrint("C++");
+    //Случай 1**: Генерация класса с одним статическим публичным методом.
+    CodeGenerator gen1(cppFactory);
+    gen1.addClass("MyClass");
+    gen1.addMethod("myMethod", "int", CppMethodUnit::STATIC, CppClassUnit::PUBLIC);
+    gen1.addPrint("Hello from C++");
+    cout << gen1.generateCode().c_str() << Qt::endl;
 
-    cout << cppGenerator.generateCode().c_str() << Qt::endl;
+    //Случай 2**: Генерация класса с виртуальным константным приватным методом.
+    CodeGenerator gen2(cppFactory);
+    gen2.addClass("MyClass1");
+    gen2.addMethod("myMethod1", "void",
+                    CppMethodUnit::VIRTUAL | CppMethodUnit::CONST,
+                    CppClassUnit::PRIVATE);
+    gen2.addPrint("test");
+    cout << gen2.generateCode().c_str() << Qt::endl;
 
-    CodeGenerator cppGenerator1(cppCodeFactory);
+    //Случай 3**: Пустое тело метода.
+    CodeGenerator gen3(cppFactory);
+    gen3.addClass("EmptyMethodClass");
+    gen3.addMethod("emptyMethod", "void", 0, CppClassUnit::PUBLIC);
+    cout << gen3.generateCode().c_str() << Qt::endl;
 
-    cppGenerator1.addClass("MyClass1");
 
-    cppGenerator1.addMethod("myMethod1", "void", CppMethodUnit::VIRTUAL | CppMethodUnit::CONST, CppClassUnit::PRIVATE);
-    cppGenerator1.addPrint("C++");
-
-    cout << cppGenerator1.generateCode().c_str() << Qt::endl;
 
     //Генерируем C# код
-    std::shared_ptr<CSharpCodeFactory> csharpCodeFactory = std::make_shared<CSharpCodeFactory>();
-    CodeGenerator csharpGenerator(csharpCodeFactory);
+    cout << "C# (CSharpCodeFactory)\n\n" << Qt::endl;
 
-    csharpGenerator.addClass("MyClass");
+    std::shared_ptr<CSharpCodeFactory> csharpFactory = std::make_shared<CSharpCodeFactory>();
 
-    csharpGenerator.addMethod("myMethod", "int", CSharpMethodUnit::STATIC, CSharpClassUnit::PUBLIC);
-    csharpGenerator.addPrint("C#");
+    //Случай 4**: Генерация публичного класса со статическим методом.
+    CodeGenerator gen4(csharpFactory);
+    gen4.addClass("MyClass", static_cast<unsigned int>(CSharpClassUnit::ClassModifier::PUBLIC));
+    gen4.addMethod("myMethod", "int", CSharpMethodUnit::STATIC, CSharpClassUnit::PUBLIC);
+    gen4.addPrint("Hello from C#");
+    cout << gen4.generateCode().c_str() << Qt::endl;
 
-    cout << csharpGenerator.generateCode().c_str() << Qt::endl;
+    //Случай 5**: Генерация internal-класса с виртуальным приватным методом.
+    CodeGenerator gen5(csharpFactory);
+    gen5.addClass("MyClass1", static_cast<unsigned int>(CSharpClassUnit::ClassModifier::INTERNAL));
+    gen5.addMethod("myMethod1", "void", CSharpMethodUnit::VIRTUAL, CSharpClassUnit::PRIVATE);
+    gen5.addPrint("test");
+    cout << gen5.generateCode().c_str() << Qt::endl;
 
-    CodeGenerator csharpGenerator1(csharpCodeFactory);
-
-    csharpGenerator1.addClass("MyClass1", static_cast<unsigned int>(CSharpClassUnit::ClassModifier::INTERNAL));
-
-    csharpGenerator1.addMethod("myMethod1", "void", CSharpMethodUnit::VIRTUAL, CSharpClassUnit::PRIVATE);
-    csharpGenerator1.addPrint("C#");
-
-    cout << csharpGenerator1.generateCode().c_str() << Qt::endl;
+    //Случай 6**: Генерация класса без модификатора доступа.
+    CodeGenerator gen6(csharpFactory);
+    gen6.addClass("DefaultClass");
+    gen6.addMethod("defaultMethod", "string", 0, CSharpClassUnit::PRIVATE);
+    gen6.addPrint("default");
+    cout << gen6.generateCode().c_str() << Qt::endl;
 
     //Генерируем Java код
-    std::shared_ptr<JavaCodeFactory> javaCodeFactory = std::make_shared<JavaCodeFactory>();
-    CodeGenerator javaGenerator(javaCodeFactory);
+    cout << "Java (JavaCodeFactory)\n\n" << Qt::endl;
 
-    javaGenerator.addClass("MyClass");
+    std::shared_ptr<JavaCodeFactory> javaFactory = std::make_shared<JavaCodeFactory>();
 
-    javaGenerator.addMethod("myMethod", "int", JavaMethodUnit::STATIC, JavaClassUnit::PUBLIC);
-    javaGenerator.addPrint("Java");
+    //Случай 7**: Генерация класса со статическим публичным методом.
+    CodeGenerator gen7(javaFactory);
+    gen7.addClass("MyClass");
+    gen7.addMethod("myMethod", "int", JavaMethodUnit::STATIC, JavaClassUnit::PUBLIC);
+    gen7.addPrint("Hello from Java");
+    cout << gen7.generateCode().c_str() << Qt::endl;
 
-    cout << javaGenerator.generateCode().c_str() << Qt::endl;
+    //Случай 8**: Генерация public abstract класса с абстрактным protected методом.
+    CodeGenerator gen8(javaFactory);
+    gen8.addClass("MyClass1",
+                 static_cast<unsigned int>(JavaClassUnit::ClassModifier::PUBLIC)
+                     | static_cast<unsigned int>(JavaClassUnit::ClassModifier::ABSTRACT));
+    gen8.addMethod("myMethod1", "void", JavaMethodUnit::ABSTRACT, JavaClassUnit::PROTECTED);
+    gen8.addPrint("abstract test");
+    cout << gen8.generateCode().c_str() << Qt::endl;
 
-    CodeGenerator javaGenerator1(javaCodeFactory);
+    //Случай 9**: Генерация package-private класса (модификатор NONE).
+    CodeGenerator gen9(javaFactory);
+    gen9.addClass("PackageClass", static_cast<unsigned int>(JavaClassUnit::ClassModifier::NONE));
+    gen9.addMethod("packageMethod", "void", 0, JavaClassUnit::PACKAGE_PRIVATE);
+    gen9.addPrint("package test");
+    cout << gen9.generateCode().c_str() << Qt::endl;
 
-    javaGenerator1.addClass("MyClass1", static_cast<unsigned int>(JavaClassUnit::ClassModifier::PUBLIC)
-                                        | static_cast<unsigned int>(JavaClassUnit::ClassModifier::ABSTRACT));
 
-    javaGenerator1.addMethod("myMethod1", "void", JavaMethodUnit::ABSTRACT, JavaClassUnit::PROTECTED);
-    javaGenerator1.addPrint("Java");
+    //Случай 10**: Создание нескольких методов с разными модификаторами доступа в одном классе.
+    //std::shared_ptr<CppCodeFactory> cppFactory = std::make_shared<CppCodeFactory>();
 
-    cout << javaGenerator1.generateCode().c_str() << Qt::endl;
+    CodeGenerator gen10(cppFactory);
+    gen10.addClass("MultiMethodClass");
+
+    gen10.addMethod("publicMethod", "void", CppMethodUnit::STATIC, CppClassUnit::PUBLIC);
+    gen10.addPrint("public");
+
+    gen10.addMethod("protectedMethod", "int", 0, CppClassUnit::PROTECTED);
+    gen10.addPrint("protected");
+
+    gen10.addMethod("privateMethod", "double", CppMethodUnit::CONST, CppClassUnit::PRIVATE);
+    gen10.addPrint("private");
+
+    cout << gen10.generateCode().c_str() << Qt::endl;
+
+    //Случай 11**: Передача некорректного значения флага доступа (выход за границы массива ACCESS_MODIFIERS).
+    CodeGenerator gen11(cppFactory);
+    gen11.addClass("InvalidFlagClass");
+    gen11.addMethod("someMethod", "void", 0, 999);
+    gen11.addPrint("test");
+    cout << gen11.generateCode().c_str() << Qt::endl;
+
 
     return 0;
 }
